2ae515957fcf16f6fa0b5939eb2ec1215f456c65
longhotsummer <longhotsummer@users.noreply.github.com>
2021-06-07 03:04:34 +0000

    Deploying to gh-pages from  @ cb854b4788c14f55aef234a584840bd3735a4811 🚀

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(function() {
+  // track outbound links
+  $('a[href^=http]').on('click', function(e) {
+    ga('send', 'event', 'outbound-click', this.href);
+  });
+});
+
+$(function() {
+  // social buttons
+  $('.fb-share').on('click', function(e) {
+    e.preventDefault();
+    var url = $(this).data('href') || location.toString();
+
+    window.open("https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(url),
+                "share", "width=600, height=400, scrollbars=no");
+    ga('send', 'social', 'facebook', 'share', url);
+  });
+
+  $('.twitter-share').on('click', function(e) {
+    e.preventDefault();
+    var url = $(this).data('href') || location.toString();
+
+    window.open("https://twitter.com/intent/tweet?" +
+                "text=" + encodeURIComponent($(this).data('text')) +
+                "&url=" + encodeURIComponent(url) +
+                "&via=OpenByLawsZA",
+                "share", "width=364, height=250, scrollbars=no");
+    ga('send', 'social', 'twitter', 'share', url);
+  });
+});
+
+$(function() {
+  var $nav = $('.about-nav');
+
+  if ($nav.length) {
+    $nav.affix({
+      offset: $nav.offset().top - 20,
+    });
+  }
+});
